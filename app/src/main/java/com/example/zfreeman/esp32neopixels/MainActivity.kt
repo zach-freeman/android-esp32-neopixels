20ec615a634d96a96221366abce5de4b58e3bb6e
Zach Freeman <zsfree00@gmail.com>
2018-10-28 11:10:53 -0400

    Replace button with switch to turn light on/off

--- app/src/main/java/com/example/zfreeman/esp32neopixels/MainActivity.kt
+++ app/src/main/java/com/example/zfreeman/esp32neopixels/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.zfreeman.esp32neopixels
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.widget.Switch
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.longToast
 import org.jetbrains.anko.uiThread
 import java.net.URL
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var button: Button
+    private lateinit var lightSwitch: Switch
     companion object {
         private const val SERVER_NAME = "espressif"
         private const val LIGHT_STATUS_PATH = "lightStatus"
@@ -21,41 +23,63 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lightSwitch = findViewById(R.id.lightSwitch)
+        initializeSwitch()
 
-        button = findViewById(R.id.button)
-        button.setOnClickListener {
-            sendLightMessage()
+        lightSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            sendLightMessage(isChecked)
         }
     }
 
-    private fun sendLightMessage()
+    private fun initializeSwitch()
     {
-            doAsync{
-                val statusResult = URL("http://$SERVER_NAME/$LIGHT_STATUS_PATH").readText()
-                uiThread {
-                    Log.wtf("Request", statusResult)
-                    longToast("Request performed")
-                }
-                if (statusResult == "On")
-                {
-                    val result = URL("http://$SERVER_NAME/$LIGHT_OFF_PATH").readText()
-                    uiThread {
-                        Log.wtf("Request", result)
-                        longToast("Request performed")
-                        button.setText("On")
-                    }
+        doAsync{
+            val statusResult = URL("http://$SERVER_NAME/$LIGHT_STATUS_PATH").readText()
 
+            if (statusResult == "On")
+            {
+                uiThread {
+                    lightSwitch.isChecked = true
                 }
-                else
-                {
-                    val result = URL("http://$SERVER_NAME/$LIGHT_ON_PATH").readText()
-                    uiThread {
-                        Log.wtf("Request", result)
-                        longToast("Request performed")
-                        button.setText("Off")
-                    }
+
+            }
+            else
+            {
+                uiThread {
+                    lightSwitch.isChecked = false
                 }
             }
+        }
+    }
 
+    private fun sendLightMessage(isChecked : Boolean)
+    {
+        if (isChecked)
+        {
+            root_layout.setBackgroundColor(Color.GREEN)
+        }
+        else
+        {
+            root_layout.setBackgroundColor(Color.LTGRAY)
+        }
+        doAsync{
+            if (isChecked)
+            {
+                val result = URL("http://$SERVER_NAME/$LIGHT_ON_PATH").readText()
+                uiThread {
+                    Log.wtf("Request", result)
+                    longToast("Light On")
+                }
+
+            }
+            else
+            {
+                val result = URL("http://$SERVER_NAME/$LIGHT_OFF_PATH").readText()
+                uiThread {
+                    Log.wtf("Request", result)
+                    longToast("Light Off")
+                }
+            }
+        }
     }
 }
