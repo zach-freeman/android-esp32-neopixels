17ffc8329b2afb9e666c5943be4cebc549774571
Zach Freeman <zsfree00@gmail.com>
2018-10-28 12:52:32 -0400

    Add christmas switch to turn on christmas themed lights

--- app/src/main/java/com/example/zfreeman/esp32neopixels/MainActivity.kt
+++ app/src/main/java/com/example/zfreeman/esp32neopixels/MainActivity.kt
@@ -13,38 +13,40 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var lightSwitch: Switch
+
     companion object {
         private const val SERVER_NAME = "espressif"
         private const val LIGHT_STATUS_PATH = "lightStatus"
         private const val LIGHT_ON_PATH = "lightOn"
         private const val LIGHT_OFF_PATH = "lightOff"
+        private const val CHRISTMAS_PATH = "christmas"
     }
+    private var isLightOn = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         lightSwitch = findViewById(R.id.lightSwitch)
-        initializeSwitch()
+        synchronizeSwitch()
 
         lightSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
             sendLightMessage(isChecked)
         }
+        christmasSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            sendChristmasMessage(isChecked)
+        }
     }
 
-    private fun initializeSwitch()
-    {
-        doAsync{
+    private fun synchronizeSwitch() {
+        doAsync {
             val statusResult = URL("http://$SERVER_NAME/$LIGHT_STATUS_PATH").readText()
 
-            if (statusResult == "On")
-            {
+            if (statusResult == "On") {
                 uiThread {
                     lightSwitch.isChecked = true
                 }
 
-            }
-            else
-            {
+            } else {
                 uiThread {
                     lightSwitch.isChecked = false
                 }
@@ -52,34 +54,45 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun sendLightMessage(isChecked : Boolean)
-    {
-        if (isChecked)
-        {
-            root_layout.setBackgroundColor(Color.GREEN)
-        }
-        else
-        {
-            root_layout.setBackgroundColor(Color.LTGRAY)
-        }
-        doAsync{
-            if (isChecked)
-            {
+    private fun sendLightMessage(isChecked: Boolean) {
+        doAsync {
+            if (isChecked) {
                 val result = URL("http://$SERVER_NAME/$LIGHT_ON_PATH").readText()
                 uiThread {
                     Log.wtf("Request", result)
                     longToast("Light On")
+                    root_layout.setBackgroundColor(Color.GREEN)
                 }
 
-            }
-            else
-            {
+            } else {
                 val result = URL("http://$SERVER_NAME/$LIGHT_OFF_PATH").readText()
                 uiThread {
                     Log.wtf("Request", result)
                     longToast("Light Off")
+                    root_layout.setBackgroundColor(Color.LTGRAY)
                 }
             }
+        }
+    }
+
+    private fun sendChristmasMessage(isChecked: Boolean) {
+        doAsync {
+            if (isChecked) {
+                val result = URL("http://$SERVER_NAME/$CHRISTMAS_PATH").readText()
+                uiThread {
+                    Log.wtf("Request", result)
+                    longToast("HO HO HO")
+                    root_layout.setBackgroundColor(Color.GREEN)
+                }
+
+        } else {
+            val result = URL("http://$SERVER_NAME/$LIGHT_OFF_PATH").readText()
+            uiThread {
+                Log.wtf("Request", result)
+                longToast("Light Off")
+                root_layout.setBackgroundColor(Color.LTGRAY)
+            }
+        }
         }
     }
 }
